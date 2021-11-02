8a9af57768aeae319cacab9090468a4187f9d2bb
jsartor7 <jsartor7@uoregon.edu>
2021-11-02 13:38:51 -0700

    restructure a bit with a new class

--- meslissaProject1/meslissaProject1/Program.cs
+++ meslissaProject1/meslissaProject1/Program.cs
@@ -2,26 +2,56 @@
 
 namespace meslissaProject1
 {
-    class Program
-    {
 
-        static string[] grabInputData()
+    public class AddressDataManager
+    {
+        string[] inputData;
+        string[] sortedData;
+        int numDistinctCodes;
+        string outputFilePath;
+        void loadInputData()
         {
             Console.WriteLine("Enter Input File Name:");
-            string inputFileName = Console.ReadLine();
-            inputFileName = @"C:\Users\craig\Documents\softwaredevelopmentposition\melissaProject\data\Buildings.txt";
-            string[] inputData = System.IO.File.ReadAllLines(inputFileName);
-            return inputData;
-        }
+            string inputFilePath = Console.ReadLine();
 
-        static string grabOutputFileHandle()
+            //no need to type this out every time for now
+            inputFilePath = @"data\InternationalProductCodes.txt";
+            inputData = System.IO.File.ReadAllLines(inputFilePath);
+            Console.WriteLine($"Loaded file: \"{inputFilePath}\" \nWith {inputData.Length} lines of data.\n");
+        }
+        void grabOutputFilePath()
         {
             //fill this in later
             Console.WriteLine("Enter Output File Name:");
-            string outputFileName = Console.ReadLine();
-            string outputFileHandle = "temp";
-            return outputFileHandle;
+            outputFilePath = Console.ReadLine();
+            outputFilePath = "temp";
         }
+
+        public void processCommandLineInputs()
+        {
+            loadInputData();
+            grabOutputFilePath();
+        }
+
+        public void sortData(bool saveData)
+        {
+            //do some stuff
+
+            if (saveData)
+            {
+
+            }
+            numDistinctCodes = inputData.Length;
+        }
+
+        public int getNumDistinctCodes()
+        {
+            return numDistinctCodes;
+        }
+    }
+
+    class Program
+    {
 
         static string[] sortInputData(string[] inputData)
         {
@@ -36,22 +66,16 @@ namespace meslissaProject1
 
         static void Main(string[] args)
         {
-            //handle input file
-            string[] inputData = grabInputData();
+            AddressDataManager dataManager = new AddressDataManager();
+            
+            //handle input and output files
+            dataManager.processCommandLineInputs();
 
-            //handle output file
-            string outputFileHandle = grabOutputFileHandle();
-
-            //sort data
-            //todo: also grab numDistinctCodes
-            string[] sortedData = sortInputData(inputData);
-
-            //save data
-            saveSortedData(outputFileHandle);
+            //sort and save data
+            dataManager.sortData(true);
 
             //write command line output
-            int numDistinctCodes = 4;
-            Console.WriteLine($"There are {numDistinctCodes} distinct product codes in the file");
+            Console.WriteLine($"There are {dataManager.getNumDistinctCodes()} distinct product codes in the file");
         }
     }
 }
