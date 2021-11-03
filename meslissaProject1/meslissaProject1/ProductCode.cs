cb2e169b217bf9af427b5eb4a7068a2d6995cd4b
jsartor7 <jsartor7@uoregon.edu>
2021-11-02 18:07:46 -0700

    split each class into a file, and clean things up with comments.

---
+++ meslissaProject1/meslissaProject1/ProductCode.cs
@@ -0,0 +1,162 @@
+﻿using System;
+
+//this class stores the data of a single product code
+public class ProductCode
+{
+    //the original text input for the product code
+    public string text;
+
+    //"text" with punctuation and spacing removed
+    public string sanitizedText;
+
+    //an array of blocks which are each either all text or numerical
+    public string[] blocks;
+
+    const int maxBlocks = 8;
+
+    //cleans out unnecessary punctuation and makes everything lowercase to generate sanitizedText from "text"
+    private void sanitizeText()
+    {
+        sanitizedText = text;
+        sanitizedText = sanitizedText.Replace(".", "");
+        sanitizedText = sanitizedText.Replace("/", "");
+        sanitizedText = sanitizedText.Replace("-", "");
+        sanitizedText = sanitizedText.Replace(" ", "");
+        sanitizedText = sanitizedText.ToLower();
+    }
+
+    //builds the array of blocks
+    private void processBlocks()
+    {
+        blocks = new string[maxBlocks];
+        int blockPos = 0;
+        bool lastType = false;
+        bool numType;
+        blocks[0] = "";
+
+        for (int i = 0; i < sanitizedText.Length; i++)
+        {
+            char currChar = sanitizedText[i];
+            numType = Char.IsDigit(currChar);
+
+            //this condition is checking if we have switched between numbers and letters
+            if (i > 0 && numType != lastType)
+            {
+                blockPos++;
+                blocks[blockPos] = "";
+            }
+
+            //we are just appending the current character to the end of our string
+            blocks[blockPos] = blocks[blockPos].Insert(blocks[blockPos].Length, currChar.ToString());
+
+            lastType = numType;
+        }
+    }
+
+
+    private int blockComparison(string block1, string block2)
+    {
+        //null blocks mean you sort before
+        if (block1 is null && block2 is null)
+        {
+            return 0;
+        }
+        else if (block1 is null && block2 is not null)
+        {
+            return -1;
+        }
+        else if (block1 is not null && block2 is null)
+        {
+            return 1;
+        }
+
+
+        int num1;
+        int num2;
+        bool block1IsNumerical = int.TryParse(block1, out num1);
+        bool block2IsNumerical = int.TryParse(block2, out num2);
+
+        //numerical blocks sort before alphabetical blocks
+        if (block1IsNumerical && !block2IsNumerical)
+        {
+            return -1;
+        }
+        else if (!block1IsNumerical && block2IsNumerical)
+        {
+            return 1;
+        }
+        else if (block1IsNumerical && block2IsNumerical)
+        {
+            //smaller numbers sort before larger numbers
+            if (num1 > num2)
+            {
+                return 1;
+            }
+            else if (num2 > num1)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        //and then finally, if they are both alphabetical, we'll run Char.CompareTo on each pair of characters
+        int sortPriority = 0;
+        for (int i = 0; i < Math.Min(block1.Length, block2.Length); i++)
+        {
+            char char1 = block1[i];
+            char char2 = block2[i];
+            sortPriority = char1.CompareTo(char2);
+            if (sortPriority != 0)
+            {
+                break;
+            }
+        }
+        if (sortPriority == 0)
+        {
+            //shorter blocks sort before longer blocks
+            if (block1.Length > block2.Length)
+            {
+                sortPriority = 1;
+            }
+            else if (block1.Length < block2.Length)
+            {
+                sortPriority = -1;
+            }
+        }
+        return sortPriority;
+    }
+
+    //this is an int because it has three values: positive if compareCode sorts before, negative if after, and 0 if equal
+    public int codeVersionComparison(ProductCode compareCode)
+    {
+        //this happens when you're comparing with the last spot in the array, which will be empty so you insert there
+        if (compareCode is null)
+        {
+            return -1;
+        }
+
+        //your sort priority will ultimately be the sort priority of the first pair of blocks that don't give 0
+        int sortPriority = 0;
+        for (int i = 0; i < maxBlocks; i++)
+        {
+            sortPriority = blockComparison(blocks[i], compareCode.blocks[i]);
+            if (sortPriority != 0)
+            {
+                break;
+            }
+        }
+
+        return sortPriority;
+    }
+
+    //constructor initializes the text, cleans it up, and forms the blocks
+    public ProductCode(string inputText)
+    {
+        text = inputText;
+        sanitizeText();
+        processBlocks();
+    }
+}
---
+++ meslissaProject1/meslissaProject1/ProductCodeDataManager.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.IO;
+
+public class ProductCodeDataManager
+{
+
+    ProductCode[] codeArray;
+    ProductCode[] sortedCodeArray;
+
+    int numDistinctCodes;
+    string outputFilePath;
+
+    void loadInputData(bool defaultPath)
+    {
+        Console.WriteLine("Enter Input File Name:");
+        string inputFilePath;
+
+        //this is just so you don't have to type it out every time
+        if (defaultPath)
+        {
+            inputFilePath = @"data\InternationalProductCodes.txt";
+        }
+        else
+        {
+            inputFilePath = Console.ReadLine();
+        }
+        string[] inputDataText = System.IO.File.ReadAllLines(inputFilePath);
+
+        //make a list of ProductCode objects rather than text
+        codeArray = new ProductCode[inputDataText.Length];
+        for (int i = 0; i < inputDataText.Length; i++)
+        {
+            codeArray[i] = new ProductCode(inputDataText[i]);
+        }
+        Console.WriteLine($"Loaded file: \"{inputFilePath}\" \nWith {inputDataText.Length} lines of data.\n");
+    }
+    void grabOutputFilePath(bool defaultPath)
+    {
+        Console.WriteLine("Enter Output File Name:");
+
+        //again just so you don't have to type it out
+        if (defaultPath)
+        {
+            outputFilePath = @"data\sortedOutput.txt";
+        }
+        else
+        {
+            outputFilePath = Console.ReadLine();
+        }
+    }
+
+    public void processCommandLineInputs(bool defaultPath)
+    {
+        loadInputData(defaultPath);
+        grabOutputFilePath(defaultPath);
+    }
+
+    //i made this private because you wouldn't want to be able to call it unless right after sorting, or it might not be sorted
+    void saveSortedData()
+    {
+        //fileIO
+        string[] lines = new string[sortedCodeArray.Length];
+        //string[] sanitizedLines = new string[sortedCodeArray.Length];
+        for (int i = 0; i < sortedCodeArray.Length; i++)
+        {
+            lines[i] = sortedCodeArray[i].text;
+            //sanitizedLines[i] = sortedCodeArray[i].sanitizedText;
+        }
+
+        File.WriteAllLinesAsync(outputFilePath, lines);
+
+        //this is just for debug purposes
+        //File.WriteAllLinesAsync(outputFilePath + ".sanitized", sanitizedLines);
+
+        Console.WriteLine($"Data saved to: \"{outputFilePath}\"\n");
+    }
+
+    //sorts the data with an "insertion sort," it's definitely sub-optimal (i.e. n^2 rather than nlogn) so depending on the system and data
+    //being processed, you could choose a more optimal sorting algorithm. insertion is just relatively straightforward to make stable
+    //alternatively for data that may be already (partially) sorted, this can be significantly more efficient than nlogn
+    public void sortData(bool saveData)
+    {
+        numDistinctCodes = 1;
+        ProductCode[] tempCodeArray;
+        sortedCodeArray = new ProductCode[codeArray.Length];
+        //go ahead and insert the first product code
+        sortedCodeArray[0] = codeArray[0];
+        int numInserted = 1;
+
+        //now iterate through the whole "codeArray" of elements to insert (start at i=1 cuz we already did one)
+        for (int i = 1; i < codeArray.Length; i++)
+        {
+            ProductCode codeToInsert = codeArray[i];
+            bool inserted = false;
+            bool isUnique = true;
+            tempCodeArray = (ProductCode[])sortedCodeArray.Clone();
+
+            //iterate through the sorted array finding the index at which to insert your element
+            //j goes to numInserted+1 because we're increasing the size of the sorted array by 1 each iteration
+            for (int j = 0; j < numInserted + 1; j++)
+            {
+                if (!inserted)
+                {
+                    ProductCode codeToCompare = tempCodeArray[j];
+                    //run our fancy comparison function, a negative sortPriority means to insert
+                    int sortPriority = codeToInsert.codeVersionComparison(codeToCompare);
+
+                    //sortPriority == 0 means they are the same code
+                    if (sortPriority == 0)
+                    {
+                        isUnique = false;
+                    }
+
+                    //the greq here ensures the "stability" of the sort - we'll insert new elements *after* eqalities
+                    if (sortPriority >= 0)
+                    {
+                        sortedCodeArray[j] = tempCodeArray[j];
+                    }
+                    else
+                    {
+                        sortedCodeArray[j] = codeToInsert;
+                        inserted = true;
+                        numInserted++;
+                        if (isUnique)
+                        {
+                            numDistinctCodes++;
+                        }
+                    }
+                }
+                else if (j < codeArray.Length)
+                {
+                    //push forward the rest of the array
+                    sortedCodeArray[j] = tempCodeArray[j - 1];
+                }
+            }
+        }
+
+
+        if (saveData)
+        {
+            saveSortedData();
+        }
+    }
+
+    public int getNumDistinctCodes()
+    {
+        return numDistinctCodes;
+    }
+}
