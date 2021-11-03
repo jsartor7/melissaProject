b87294c0ed01b9a3d584a8d22fb599316079728f
jsartor7 <jsartor7@uoregon.edu>
2021-11-02 20:33:28 -0700

    everthing working to stdout

--- melissaProject2/melissaProject2/AddressManager.cs
+++ melissaProject2/melissaProject2/AddressManager.cs
@@ -18,14 +18,71 @@ public class AddressManager
 		streetsDataArray = new DataArray("data/Streets.txt");
 		buildingsDataArray = new DataArray("data/Buildings.txt");
 		apartmentsDataArray = new DataArray("data/Apartments.txt");
-		Console.WriteLine("Hello World!");
-		Console.WriteLine(cityDataArray.elemList[0].dataList[0]);
 	}
 
+	private void combineArrays()
+    {
+		string[] propertyList = { "StateName", "CityName", "PostalCode", "StreetName", "DeliveryPoints" };
+
+
+		DataElem[] streetList = streetsDataArray.elemList;
+		string[] output = new string[streetList.Length];
+		for (int i = 0; i < streetList.Length; i++)
+		{
+			string outStr = "";
+			DataElem currStreet = streetList[i];
+			DataElem cityRow = cityDataArray.filter("CityId", currStreet.grabProperty("CityId"))[0];
+			string stateName = cityRow.grabProperty("StateName");
+			outStr = outStr + stateName + "|";
+
+			string cityName = cityRow.grabProperty("CityName");
+			outStr = outStr + cityName + "|";
+
+			string postalCode = currStreet.grabProperty("PostalCode");
+			outStr = outStr + postalCode + "|";
+
+			string streetName = currStreet.grabProperty("StreetName");
+			outStr = outStr + streetName + "|";
+
+			DataElem[] buildingsList = buildingsDataArray.filter("StreetId", currStreet.grabProperty("StreetId"));
+			for(int j=0; j<buildingsList.Length; j++)
+            {
+				DataElem currBuilding = buildingsList[j];
+				outStr = outStr + currBuilding.grabProperty("BuildingNumber");
+
+				DataElem[] apartmentList = apartmentsDataArray.filter("BuildingId", currBuilding.grabProperty("BuildingId"));
+				if (apartmentList.Length>0)
+				{
+					outStr = outStr + "(";
+					for (int k = 0; k < apartmentList.Length; k++)
+					{
+						outStr = outStr + apartmentList[k].grabProperty("ApartmentNumber");
+						if (k < apartmentList.Length - 1)
+                        {
+							outStr = outStr + ",";
+                        }
+					}
+						outStr = outStr + ")";
+				}
+
+				if (j < buildingsList.Length - 1)
+				{
+					outStr = outStr + ";";
+				}
+			}
+
+
+			Console.WriteLine(outStr);
+
+		}
+
+    }
+	
 
 
 	public AddressManager()
 	{
 		loadArrays();
+		combineArrays();
 	}
 }
--- melissaProject2/melissaProject2/DataArray.cs
+++ melissaProject2/melissaProject2/DataArray.cs
@@ -17,4 +17,23 @@ public class DataArray
             elemList[i-1] = new DataElem(names,data);
         }
     }
+
+    public DataElem[] filter(string name, string value)
+    {
+        //this is quite obviously a wildly inefficient use of resources
+        //some kind of dynamic array sizing would be a lot better
+        DataElem[] filteredList = new DataElem[elemList.Length];
+        int currIndex = 0;
+
+        for (int i = 0; i < elemList.Length; i++)
+        {
+            if (elemList[i].grabProperty(name) == value)
+            {
+                filteredList[currIndex] = elemList[i];
+                currIndex++;
+            }
+        }
+        Array.Resize(ref filteredList, currIndex);
+        return filteredList;
+    }
 }
